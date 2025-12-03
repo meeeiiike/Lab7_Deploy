c47680c2f2c367d3c078ae7bab1a842f626a5c26
G00407095@atu.ie <G00407095@atu.ie>
2025-12-03 15:40:13 +0000

    init commit of deploy repo with .env file

---
+++ src/main/java/ie/atu/lab7/lab7_deploy/Lab7DeployApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.lab7.lab7_deploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab7DeployApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab7DeployApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/lab7/lab7_deploy/Lab7DeployApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.lab7.lab7_deploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab7DeployApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
